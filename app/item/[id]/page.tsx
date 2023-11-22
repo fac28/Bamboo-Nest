f92ed4e8b0dd65224c12d1b14ead312bd1eb437d
JamesESS <jesandfordsmith@gmail.com>
2023-11-22 16:27:54 +0000

    resolved type errors add some error handling

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -3,52 +3,53 @@ import { PostgrestError } from '@supabase/supabase-js'
 import { cookies } from 'next/headers'
 
 const itemQuery =
-  'name,price,description,brand,delivery,collection, age(age_category), categories(category_name), conditions(condition,description)'
+  '*, age(age_category), categories(category_name), conditions(condition,description)'
 
-  type ItemType = {
-    name: string;
-    price: number;
-    description: string;
-    brand: string;
-    delivery: boolean;
-    collection: boolean;
-    age: {
-      age_category: string;
-    };
-    categories: {
-      category_name: string;
-    };
-    conditions: {
-      condition: string;
-      description: string;
-    };
-  };
+type ItemType = {
+  name: string
+  price: number
+  description: string
+  brand: string
+  delivery: boolean
+  collection: boolean
+  age: {
+    age_category: string
+  }
+  categories: {
+    category_name: string
+  }
+  conditions: {
+    condition: string
+    description: string
+  }
+}
 
 export default async function listing({
   params,
 }: {
   params: {
-    id: string;
-  };
+    id: string
+  }
 }) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
 
   try {
-    const { data, error }: { data: ItemType[] | null; error: PostgrestError } = await supabase
+    const { data, error } = await supabase
       .from('items')
       .select(itemQuery)
-      .eq('item_id', params.id);
-
-    if (error || !data) {
-      throw new Error('Error fetching data');
+      .eq('item_id', params.id)
+    console.log(error,data)
+    if (error || !data || data.length === 0) {
+      // need to split data.length conditional into own if leading to 404
+      throw new Error('Error fetching data')
     }
-
-    const { name, price, description, brand, delivery, collection } = data && data[0];
-    const age = data && data[0]?.age?.age_category;
-    const condition = data && data[0]?.conditions?.condition;
-    const conditionDescription = data && data[0]?.conditions?.description;
-    const category = data && data[0]?.categories?.category_name;
+    const { name, price, description, brand, delivery, collection } =
+      data && data[0]
+    const age = data && data[0]?.age?.age_category
+    const condition = data && data[0]?.conditions?.condition
+    const conditionDescription = data && data[0]?.conditions?.description
+    const category = data && data[0]?.categories?.category_name
 
     return (
       <>
@@ -64,10 +65,9 @@ export default async function listing({
         <p>Item condition expanded: {conditionDescription}</p>
         <p>Item category: {category}</p>
       </>
-    );
+    )
   } catch (error) {
-    console.log(error);
-    // Handle the error as needed
-    return <p>Error loading data</p>;
+    console.log(error)
+    return <p>Error loading data</p>
   }
 }
