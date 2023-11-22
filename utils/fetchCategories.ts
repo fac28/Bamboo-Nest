d06fcfc1981abf56c92ee009cc712f7a9d1c18da
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-11-22 14:57:45 +0000

    linted

--- utils/fetchCategories.ts
+++ utils/fetchCategories.ts
@@ -2,16 +2,14 @@ import { cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
 
 export default async function fetchCategories() {
+  const cookieStore = cookies()
+  const supabase = createClient(cookieStore)
 
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
+  const { data, error } = await supabase.from('categories').select()
 
-    const { data, error } = await supabase.from('categories').select()
-
-    if (error) {
-        console.log(error)
-        return []
-    }
-    return data
-
+  if (error) {
+    console.log(error)
+    return []
+  }
+  return data
 }
