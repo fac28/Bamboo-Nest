4a4ae4d4dec5ed7ea708df32cff26069d2b52118
isobelbutler <isobutler@icloud.com>
2023-11-22 17:54:42 +0000

    Header functional but not styled

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,71 +1,11 @@
-'use client'
-// import AuthButton from './AuthButton'
+import AuthButton from '@/components/AuthButton'
+import NavigationMenu from '@/components/NavigationMenu'
 
-import React from 'react'
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  // NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  // Button,
-} from '@nextui-org/react'
-
-export default function Header() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false)
-
-  const menuItems = [
-    'Profile',
-    'Dashboard',
-    'Activity',
-    'Analytics',
-    'System',
-    'Deployments',
-    'My Settings',
-    'Team Settings',
-    'Help & Feedback',
-    'Log Out',
-  ]
-
+export default async function Header() {
   return (
-    <Navbar onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarBrand>
-          <p className="font-bold text-inherit">Bamboo Nest</p>
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent justify="end">
-        {/* <AuthButton/> */}
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-          // className="sm:hidden"
-        />
-        
-      </NavbarContent>
-      <NavbarMenu>
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2
-                  ? 'primary'
-                  : index === menuItems.length - 1
-                    ? 'danger'
-                    : 'foreground'
-              }
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+    <div className="flex">
+      <NavigationMenu />
+      <AuthButton />
+    </div>
   )
 }
