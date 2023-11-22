59eb5f5424b6772627ff87c24fa4a5522c1b5872
JamesESS <jesandfordsmith@gmail.com>
2023-11-22 17:54:00 +0000

    display image from bucket

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -1,9 +1,9 @@
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
+import Image from 'next/image'
 
 const itemQuery =
   '*, age(age_category), categories(category_name), conditions(condition,description)'
-
 
 export default async function listing({
   params,
@@ -20,7 +20,7 @@ export default async function listing({
       .from('items')
       .select(itemQuery)
       .eq('item_id', params.id)
-    console.log(error,data)
+    // console.log(error, data)
     if (error || !data || data.length === 0) {
       // need to split data.length conditional into own if leading to 404
       throw new Error('Error fetching data')
@@ -31,7 +31,15 @@ export default async function listing({
     const condition = data && data[0]?.conditions?.condition
     const conditionDescription = data && data[0]?.conditions?.description
     const category = data && data[0]?.categories?.category_name
-
+    const imageUUID = data && data[0]?.image_id
+    const allImages = await supabase.storage.from('item-pictures').list('')
+    const imageName =
+      allImages &&
+      allImages.data?.filter(singleImage => singleImage.id === imageUUID)
+    const publicUrl = await supabase.storage
+      .from('item-pictures')
+      .getPublicUrl(`${imageName && imageName[0].name}`)
+    console.log(publicUrl)
     return (
       <>
         <h1>{name}</h1>
@@ -45,6 +53,7 @@ export default async function listing({
         <p>Item condition: {condition}</p>
         <p>Item condition expanded: {conditionDescription}</p>
         <p>Item category: {category}</p>
+        <img src={publicUrl.data.publicUrl} width={500} height={500} />
       </>
     )
   } catch (error) {
