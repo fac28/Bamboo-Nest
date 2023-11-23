e0b4f51b85ab7d9723e2806c355b0b125b33f813
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-11-23 15:12:36 +0000

    linted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-import { GeistSans } from 'geist/font/sans';
-import './globals.css';
-import { Providers } from './providers';
-import 'normalize.css/normalize.css';
+import { GeistSans } from 'geist/font/sans'
+import './globals.css'
+import { Providers } from './providers'
+import 'normalize.css/normalize.css'
 import Header from '@/components/Header'
-
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
