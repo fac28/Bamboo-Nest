ce4b472e9fd2c67706a2d34bb9ca6c8f4e245355
James Sandford Smith <jesandfordsmith@gmail.com>
2023-11-23 13:04:25 +0000

    refactor

--- components/FavouriteButton.tsx
+++ components/FavouriteButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createClient } from '@/utils/supabase/client'
+import togggleFavourite from "@/utils/handleFavouriteItem"
 
 export default function FavouriteButton({
   user,
@@ -16,53 +16,4 @@ export default function FavouriteButton({
   ) : (
     <p>'not logged in'</p>
   )
-}
-const supabase = createClient()
-
-async function togggleFavourite(user: string, itemID: string) {
-  try {
-    const { data, error } = await supabase
-      .from('users')
-      .select('favourite_items')
-      .eq('id', user)
-    const favouriteItems: string[] | null = data && data[0].favourite_items
-    if (error || !data || data.length === 0) {
-      throw new Error('Error fetching data')
-    }
-    if (!favouriteItems || favouriteItems.length === 0) {
-      updateSupabaseFavouriteItems(user, [itemID])
-      return
-    }
-    if (favouriteItems.includes(itemID)) {
-      const updatedFavouriteItems = favouriteItems.filter(
-        item => item !== itemID,
-      )
-      console.log('remove item', { updatedFavouriteItems })
-      updateSupabaseFavouriteItems(user, updatedFavouriteItems)
-      return
-    }
-    const updatedFavouriteItems = [...favouriteItems, itemID]
-    console.log('add item', { updatedFavouriteItems })
-    updateSupabaseFavouriteItems(user, updatedFavouriteItems)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function updateSupabaseFavouriteItems(
-  user: string,
-  updatedItems: string[],
-) {
-  try {
-    const { data, error } = await supabase
-      .from('users')
-      .update({ favourite_items: updatedItems })
-      .eq('id', user)
-      .select()
-    if (error || !data) {
-      throw new Error('Error fetching data')
-    }
-  } catch (error) {
-    console.log(error)
-  }
 }
