919d85b53dd331aa362c8588164bdac56c931b3b
JamesESS <jesandfordsmith@gmail.com>
2023-11-24 11:35:10 +0000

    add some html structure & display avatar

--- app/seller/[id]/page.tsx
+++ app/seller/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { createClient } from '@/utils/supabase/server'
 import { cookies } from 'next/headers'
 
-
 export default async function listing({
   params,
 }: {
@@ -19,24 +18,47 @@ export default async function listing({
     if (error || !data || data.length === 0) {
       throw new Error('Error fetching data')
     }
-    console.log(data)
-    const {first_name, last_name, bio, sale_history, items_for_sale, profile_picture} = data[0]
+    const {
+      first_name,
+      last_name,
+      bio,
+      sale_history,
+      items_for_sale,
+      profile_picture,
+    } = data[0]
     const fullName = `${first_name} ${last_name}`
-    return (<>
-    <div>
-        <h1>{fullName}</h1>
-        <img src={profile_picture&&profile_picture}></img>
-    </div>
+
+    const { data: allImagesData } = await supabase.storage
+      .from('profile-pictures')
+      .list('')
+    const imageName = allImagesData?.filter(
+      singleImage => singleImage.id === profile_picture,
+    )
+    const { data: publicUrl } = await supabase.storage
+      .from('profile-pictures')
+      .getPublicUrl(`${imageName && imageName[0].name}`)
+    return (
+      <>
+        <div>
+          <h1>{fullName}</h1>
+          <img
+            src={publicUrl.publicUrl}
+            alt={`${fullName}'s avatar photo`}
+          ></img>
+        </div>
         <p>{bio}</p>
         <div>
-            <p>{items_for_sale&&items_for_sale.length} items for sale</p>
-            <button>see all</button>
+          <p>{items_for_sale && items_for_sale.length} items for sale</p>
+          {/* want to add a card or list of cards here with most recent items */}
+          <button>see all</button>
         </div>
         <div>
-            <p>{sale_history&&sale_history.length} items sold</p>
-            <button>see all</button>
+          <p>{sale_history && sale_history.length} items sold</p>
+          {/* want to add a card or list of cards here with most recent items */}
+          <button>see all</button>
         </div>
-    </>)
+      </>
+    )
   } catch (error) {
     console.log(error)
     return <p>Error loading data</p>
