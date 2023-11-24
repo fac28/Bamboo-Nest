4e8a1fdbb618ebf162bdb2d030e33bd6e830193f
JamesESS <jesandfordsmith@gmail.com>
2023-11-24 15:14:35 +0000

    format

--- app/products/[category]/page.tsx
+++ app/products/[category]/page.tsx
@@ -3,7 +3,6 @@ import fetchImage from '@/utils/fetchImage'
 import PageContainer from '@/components/PageContainer'
 import Link from 'next/link'
 
-
 export default async function Page({
   params,
 }: {
@@ -21,17 +20,17 @@ export default async function Page({
         <div className="grid grid-cols-3 gap-4">
           {items.map(item => (
             <Link href={`/item/${item.item_id}`} key={item.name}>
-            <div className="bg-white shadow-md rounded-md p-4">
-              <h1 className="text-2xl font-bold">{item.name}</h1>
-              <p className="text-xl">£{item.price}</p>
-              <img
-                src={item.image}
-                width={200}
-                height={200}
-                alt={`image of ${item.name}`}
-              />
-            </div>
-          </Link>
+              <div className="bg-white shadow-md rounded-md p-4">
+                <h1 className="text-2xl font-bold">{item.name}</h1>
+                <p className="text-xl">£{item.price}</p>
+                <img
+                  src={item.image}
+                  width={200}
+                  height={200}
+                  alt={`image of ${item.name}`}
+                />
+              </div>
+            </Link>
           ))}
         </div>
       </div>
