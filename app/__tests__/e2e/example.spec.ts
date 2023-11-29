9dab8c51ffdf8e3dc1e4c88bcbe66ffad25ad738
Dylan <122552492+dylancobb@users.noreply.github.com>
2023-11-29 15:27:58 +0000

    Fix comments on first test, add two new tests

--- app/__tests__/e2e/example.spec.ts
+++ app/__tests__/e2e/example.spec.ts
@@ -1,10 +1,25 @@
 import { test, expect } from '@playwright/test'
 
-test('should navigate to the about page', async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+test('should navigate to the login page', async ({ page }) => {
   await page.goto('/')
-  // Find an element with the text 'About Page' and click on it
-  await page.click('text=Login')
-  // The new URL should be "/about" (baseURL is used there)
+  // Find an element with the text 'Login' and click on it
+  await page.getByRole('link', { name: 'Login' }).click();
+  // The new URL should be "/login"
   await expect(page).toHaveURL('/login')
+})
+
+test('should navigate to the search page', async ({ page }) => {
+  await page.goto('/')
+  // Find a link with the text 'Buy' and click on it
+  await page.getByRole('link', { name: 'Buy' }).click();
+  // The new URL should be "/search"
+  await expect(page).toHaveURL('/search')
+})
+
+test('should navigate to the upload page', async ({ page }) => {
+  await page.goto('/')
+  // Find a link with the text 'Sell' and click on it
+  await page.getByRole('link', { name: 'Sell' }).click();
+  // The new URL should be "/upload"
+  await expect(page).toHaveURL('/upload')
 })
