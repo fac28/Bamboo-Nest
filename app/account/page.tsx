51fb04c632e92f056eebaf189ed81676bfba7321
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-11-30 14:42:33 +0000

    refactored

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 import PageContainer from '@/components/PageContainer'
 import getUser from '@/utils/getUser'
 import { Card, CardHeader } from '@nextui-org/react'
+import { linkData } from '@/utils/constants'
 
 export default async function Login() {
+
   const { user } = await getUser()
   if (!user) {
     return (
@@ -14,55 +16,19 @@ export default async function Login() {
   }
   return (
     <PageContainer>
-      <h1> Your Account </h1>
+      <h1>Your Account</h1>
       <div className="gap-4 grid grid-cols-2 grid-rows-2">
-        <Link href="/account/favourites">
-          <Card
-            className={`border-2 border-solid border-primaryBlue rounded-3xl h-20`}
-          >
-            <CardHeader className="flex items-center justify-center h-full">
-              <h4 className="text-sm text-primaryBlue font-medium lg:text-xl">
-                Favourites
-              </h4>
-            </CardHeader>
-          </Card>
-        </Link>
-
-        <Link href="/account/purchases">
-          <Card
-            className={`border-2 border-solid border-primaryBlue rounded-3xl h-20`}
-          >
-            <CardHeader className="flex items-center justify-center h-full">
-              <h4 className="text-sm text-primaryBlue font-medium lg:text-xl">
-                Purchases
-              </h4>
-            </CardHeader>
-          </Card>
-        </Link>
-
-        <Link href="/account/listings">
-          <Card
-            className={`border-2 border-solid border-primaryBlue rounded-3xl h-20`}
-          >
-            <CardHeader className="flex items-center justify-center h-full">
-              <h4 className="text-sm text-primaryBlue font-medium lg:text-xl">
-                All Listings
-              </h4>
-            </CardHeader>
-          </Card>
-        </Link>
-
-        <Link href="/account/details">
-          <Card
-            className={`border-2 border-solid border-primaryBlue rounded-3xl h-20`}
-          >
-            <CardHeader className="flex items-center justify-center h-full">
-              <h4 className="text-sm text-primaryBlue font-medium lg:text-xl">
-                Personal Details
-              </h4>
-            </CardHeader>
-          </Card>
-        </Link>
+        {linkData.map(({ href, text }) => (
+          <Link href={href} key={href}>
+            <Card className="border-2 border-solid border-primaryBlue rounded-3xl h-20">
+              <CardHeader className="flex items-center justify-center h-full">
+                <h4 className="text-sm text-primaryBlue font-medium lg:text-xl">
+                  {text}
+                </h4>
+              </CardHeader>
+            </Card>
+          </Link>
+        ))}
       </div>
     </PageContainer>
   )
---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+export const linkData = [
+  { href: '/account/favourites', text: 'Favourites' },
+  { href: '/account/purchases', text: 'Purchases' },
+  { href: '/account/listings', text: 'All Listings' },
+  { href: '/account/details', text: 'Personal Details' },
+];
