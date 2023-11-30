946f1757a8f2d477b7d29cc6f6f9696754a39689
Dylan <122552492+dylancobb@users.noreply.github.com>
2023-11-30 13:24:16 +0000

    Update index tests + other  minor test refactoring

--- app/__tests__/e2e/menu.spec.ts
+++ app/__tests__/e2e/menu.spec.ts
@@ -1,46 +1,51 @@
 import { test, expect } from '@playwright/test'
 
+// test logo button
+test('should navigate to the home page', async ({ page }) => {
+  await page.goto('http://localhost:3000/search');
+  await page.getByRole('link', { name: 'Bamboo Nest' }).click();
+  await expect(page).toHaveURL('/')
+});
+
+//test login button
+test('should navigate to the login page', async ({ page }) => {
+  await page.goto('/')
+  await page.getByRole('link', { name: 'Login' }).click()
+  await expect(page).toHaveURL('/login')
+})
+
+// tests for burger menu
 test('should navigate to the search page (burger menu)', async ({ page }) => {
   await page.goto('/')
-  // Open the menu and click the Search Items button
   await page.getByLabel('Open menu').click()
   await page.getByRole('link', { name: 'Search Items' }).click()
-  // The new URL should be "/search"
   await expect(page).toHaveURL('/search')
 })
 
 test('should navigate to the upload page (burger menu)', async ({ page }) => {
   await page.goto('/')
-  // Open the menu and click the Sell Item button
   await page.getByLabel('Open menu').click()
   await page.getByRole('link', { name: 'Sell Item' }).click()
-  // The new URL should be "/upload"
   await expect(page).toHaveURL('/upload')
 })
 
 test('should navigate to the account page (burger menu)', async ({ page }) => {
   await page.goto('/')
-  // Open the menu and click the Account button
   await page.getByLabel('Open menu').click()
   await page.getByRole('link', { name: 'Account' }).click()
-  // The new URL should be "/account"
   await expect(page).toHaveURL('/account')
 })
 
 test('should navigate to the contact page (burger menu)', async ({ page }) => {
   await page.goto('/')
-  // Open the menu and click the Contact button
   await page.getByLabel('Open menu').click()
   await page.getByRole('link', { name: 'Contact' }).click()
-  // The new URL should be "/account"
   await expect(page).toHaveURL('/contact')
 })
 
 test('should navigate to the FAQ page (burger menu)', async ({ page }) => {
   await page.goto('/')
-  // Open the menu and click the FAQs button
   await page.getByLabel('Open menu').click()
   await page.getByRole('link', { name: 'FAQs' }).click()
-  // The new URL should be "/faq"
   await expect(page).toHaveURL('/faq')
 })
