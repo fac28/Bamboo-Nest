5eb3bca4ae21c8ec21b28926506a4b9bfcec4aba
Dylan <122552492+dylancobb@users.noreply.github.com>
2023-11-30 11:37:26 +0000

    Fix navbar logo not linking to home page

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default async function Header() {
   return (
     <div className="flex items-center sticky top-0 z-30 bg-white w-full">
-      <Link href="#">
+      <Link href="/">
         <p className="font-bold text-inherit px-6 text-xl">Bamboo Nest</p>
       </Link>
 
