25367035b5062aa8670e9deb7e93354d558fd6fc
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-12-03 23:13:48 +0000

    small refactors

--- components/ListingHistory.tsx
+++ components/ListingHistory.tsx
@@ -1,7 +1,9 @@
 import fetchItemsBySeller from '@/utils/fetchItemsBySeller'
-import { ItemWithImage } from '@/utils/types'
+import { ItemWithImage, Review } from '@/utils/types'
 import ItemCard from '@/components/ItemCard'
 import getUser from '@/utils/getUser'
+import DisplayReview from '@/components/DisplayReview'
+import fetchReviewBySeller from '@/utils/fetchReviewBySeller'
 
 export default async function ListingHistory({ id = '' }) {
   const { user, supabase } = await getUser()
@@ -10,9 +12,13 @@ export default async function ListingHistory({ id = '' }) {
   if (id === '') id = userID
 
   const itemDetails: ItemWithImage[] = await fetchItemsBySeller(supabase, id)
+  const reviewData: Review[] = await fetchReviewBySeller(supabase, id)
 
   return itemDetails.length > 0 ? (
     <div className="flex flex-col gap-4 py-16">
+      <h1>Reviews you have received</h1>
+      <DisplayReview reviewData={reviewData }/>
+      <h1>All Listings</h1>
       <div className="grid grid-cols-2 gap-4">
         {itemDetails.map(item => (
           <div key={item.item_id}>
