771dd42caf03cd205ed21ee20ff7d6388f3ebb25
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-12-05 15:58:59 +0000

    format

--- components/UploadImage.tsx
+++ components/UploadImage.tsx
@@ -1,34 +1,33 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Image from 'next/image'
 
-export default function PreviewImage({profile}: {profile: string}) {
-
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const [imagePreview, setImagePreview] = useState<string>('');
+export default function PreviewImage({ profile }: { profile: string }) {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null)
+  const [imagePreview, setImagePreview] = useState<string>('')
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+    const file = event.target.files?.[0]
     if (file) {
-    const file = event.target.files?.[0];
-    if (file) {
-      setSelectedImage(file);
+      const file = event.target.files?.[0]
+      if (file) {
+        setSelectedImage(file)
 
-      const imageURL = URL.createObjectURL(file);
-      setImagePreview(imageURL);
+        const imageURL = URL.createObjectURL(file)
+        setImagePreview(imageURL)
+      }
     }
-  }
   }
 
   return (
     <>
       <label htmlFor="profile-picture">Profile Picture</label>
       <Image
-          src={selectedImage? imagePreview: profile}
-          width={100}
-          height={100}
-          alt="Preview"
+        src={selectedImage ? imagePreview : profile}
+        width={100}
+        height={100}
+        alt="Preview"
       />
       <input
         type="file"
