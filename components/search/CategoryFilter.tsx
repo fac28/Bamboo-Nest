c9f17e96668a63e61453542f845e49adc19c5412
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-12-06 13:53:26 +0000

    filter by cat

---
+++ components/search/CategoryFilter.tsx
@@ -0,0 +1,30 @@
+'use client'
+import SelectCategories from '@/components/forms/CategoryDropDown'
+import { CategoryFilterProps } from '@/utils/types'
+
+export default function CategoryFilter(
+  {categories,
+  subCategories,
+  selectedCategoryState,
+  setSelectedCategoryState,
+  selectedSubCategoryState,
+  setSelectedSubCategoryState,
+}:
+  CategoryFilterProps
+  ) {
+
+  return (
+    <div>
+      <SelectCategories
+          categories={categories}
+          subCategories={subCategories}
+          className={'custom-input'}
+          selectedCategoryState={selectedCategoryState}
+          setSelectedCategoryState={setSelectedCategoryState}
+          selectedSubCategoryState={selectedSubCategoryState}
+          setSelectedSubCategoryState={setSelectedSubCategoryState}
+        />
+
+    </div>
+  )
+}
--- components/search/ShowFilteredResults.tsx
+++ components/search/ShowFilteredResults.tsx
@@ -7,21 +7,31 @@ export default function FilteredResults({
   favouriteItems,
   user,
   sortByPrice,
+  category_id,
+  sub_category_id,
 }: FilteredResultsProps) {
+
+  const filteredResults = searchResults.filter(
+    result =>
+      result.price >= filterPrice[0] && result.price <= filterPrice[1],
+    ).sort((a, b) => {
+    return sortByPrice === 'descending'
+      ? b.price - a.price
+      : sortByPrice === 'ascending'
+        ? a.price - b.price
+        : 1
+    }
+    ).filter(result => {
+      return category_id ? result.category_id === category_id : true
+    })
+    .filter(result => {
+      return sub_category_id ? result.sub_category_id === sub_category_id : true
+    })
+
+
   return (
     <div className="mt-4 grid grid-cols-2 gap-4">
-      {searchResults
-        .filter(
-          result =>
-            result.price >= filterPrice[0] && result.price <= filterPrice[1],
-        )
-        .sort((a, b) => {
-          return sortByPrice === 'descending'
-            ? b.price - a.price
-            : sortByPrice === 'ascending'
-              ? a.price - b.price
-              : 1
-        })
+      {filteredResults.length>0 ? filteredResults
         .map(result => (
           <div key={result.item_id}>
             <ItemCard
@@ -30,7 +40,7 @@ export default function FilteredResults({
               user={user}
             />
           </div>
-        ))}
+        )): <p>No Match</p>}
     </div>
   )
 }
